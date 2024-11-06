b041b30fe98369da4db62359ba413f5a68f66c2c
dustindo129 <159583730+dustindo129@users.noreply.github.com>
2024-11-06 15:22:11 -0600

    Update db.js
    Add comments

--- api/db.js
+++ api/db.js
@@ -1,12 +1,13 @@
+// Import the MySQL library to interact with MySQL databases
 import mysql from 'mysql2';
 
-
+// Create a connection to the MySQL database using specified credentials
 export const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "Blueshoe1",
-    database: "CollabCampus",
+    host: "localhost", // Database server host (local machine in this case)
+    user: "root", // Username for database access
+    password: "Blueshoe1", // Password for database access
+    database: "CollabCampus", // Name of the database to connect to
     authPlugins: {
-        mysql_native_password: true,
+        mysql_native_password: true, // Use MySQL's native password plugin for authentication
     }
 }); 
